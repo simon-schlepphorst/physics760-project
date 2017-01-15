8c3e48b72b6ea75eb42b2c14f4a0708ef67cff2b
BrutalPoseidon <rico@uni-bonn.de>
2017-01-15 20:25:59 +0100

    Additions
    Added an ACF and energy calculation for Energy.
    Much work needs to be done I'm just putting it up for safekeeping.

--- python/ising.py
+++ python/ising.py
@@ -30,15 +30,33 @@ def n_step_pic(T,i,Arr,n):
     else:
         return
 
+def ACF(array,swep):
+    C = np.zeros_like(array)
+    for y,x in enumerate(array):
+        for i in x:
+            C[y,int(i)] = (array[y,0] * array[y,int(i)] - np.mean(array[y, :])**2)
+    return C
+    
+def init_energy(spin_array, lattice):
+    E = np.zeros_like(spin_array)
+    for x in range(lattice):
+        for y in range(lattice):
+            E[x,y] = 2 * spin_array[x, y] * sum(find_neighbors(spin_array, lattice, x, y))
+    return E.mean()
+
 #Plot parameters
 cmap = mpl.colors.ListedColormap(['black','white'])
 bounds=[-1,0,1]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
 
-RELAX_SWEEPS = 50
-lattice = input("Enter lattice size: ")
-sweeps = input("Enter the number of Monte Carlo Sweeps: ")
+
+lattice = int(input("Enter lattice size: "))
+sweeps = int(input("Enter the number of Monte Carlo Sweeps: "))
+RELAX_SWEEPS = int(sweeps/100)
+ACFE = np.zeros((50,sweeps + RELAX_SWEEPS))
+ACFM = np.zeros((50,sweeps + RELAX_SWEEPS))
+
 
 
 if os.path.isdir('Images') is False:
@@ -72,13 +90,14 @@ def SS():
 def RS():
     T = []
     M = []
-    steps = input("Enter how many steps in between images (set to 1 if every picture is wanted): ")
+    steps = int(input("Enter how many steps in between images (set to 1 if every picture is wanted): "))
     for temperature in np.arange(0.1, 5.0, 0.1):
         if os.path.isdir('Images/T-'+str(temperature)) is True:
             continue
         if os.path.isdir('Images/T-'+str(temperature)) is False:
             os.mkdir('Images/T-'+str(temperature))
         spin_array = init_spin_array(lattice)
+        E = init_energy(spin_array, lattice)
         mag = np.zeros(sweeps + RELAX_SWEEPS)
         for sweep in range(sweeps + RELAX_SWEEPS):
             
@@ -96,14 +115,22 @@ def RS():
             n_step_pic(temperature,sweep,spin_array,steps)
             
             mag[sweep] = abs(sum(sum(spin_array))) / (lattice ** 2)
-
+            E = E + e
+            ACFE[int(temperature*10 - 1),sweep] = E
+            ACFM[int(temperature*10 - 1),sweep] = mag[sweep]
+            
         T = T + [temperature]
         M = M + [sum(mag[RELAX_SWEEPS:]) / sweeps]
-        print(temperature, sum(mag[RELAX_SWEEPS:]) / sweeps , len(M), len(T))
-        #print(temperature)
-        #print(sum(mag[RELAX_SWEEPS:]) / sweeps)
-        #print(len(M))
-        #print(len(T))
+        print(temperature, sum(mag[RELAX_SWEEPS:]) / sweeps)
+    
+    c_e = ACF(ACFE,sweeps + RELAX_SWEEPS)
+    c_m = ACF(ACFM,sweeps + RELAX_SWEEPS)
+    
+    print(T)
+    print(M)
+    print(ACFE)
+    print(c_e)
+    
     fig = plt.figure(1)
     plt.plot(T,M,'b-*',label='Data')
     plt.title('Magnetization vs Temperature')
@@ -111,5 +138,38 @@ def RS():
     plt.ylabel('Magnetization')
     fig.tight_layout()
     plt.show()
+    
+    fig = plt.figure()
+    plt.plot(range(len(c_e[0])),c_e[0],'b-*',label='T = 0.1')
+    plt.plot(range(len(c_e[0])),c_e[9],'r-o',label='T = 1.0')
+    plt.plot(range(len(c_e[0])),c_e[19],'k-^',label='T = 2.0')
+    plt.plot(range(len(c_e[0])),c_e[29],'c-s',label='T = 3.0')
+    plt.plot(range(len(c_e[0])),c_e[39],'m-p',label='T = 4.0')
+    plt.title('ACF of Energy')
+    plt.xlabel('Time Step')
+    plt.ylabel('ACF Value')
+    fig.tight_layout()
+    plt.legend(loc='best')
+    plt.show()
+    
+    fig = plt.figure()
+    plt.plot(range(len(c_m[0])),c_m[0],'b-*',label='T = 0.1')
+    plt.plot(range(len(c_m[0])),c_m[9],'r-o',label='T = 1.0')
+    plt.plot(range(len(c_m[0])),c_m[19],'k-^',label='T = 2.0')
+    plt.plot(range(len(c_m[0])),c_m[29],'c-s',label='T = 3.0')
+    plt.plot(range(len(c_m[0])),c_m[39],'m-p',label='T = 4.0')
+    plt.title('ACF of Magnetization')
+    plt.xlabel('Time Step')
+    plt.ylabel('ACF Value')
+    fig.tight_layout()
+    plt.legend(loc='best')
+    plt.show()
+    
+    #pl1 = ["ACFE","c_e","Temp","Mag"]
+    #pl2 = [ACFE,c_e,T,M]
+    #for i,j in zip(pl1,pl2):
+    #    np.savetxt(i+".txt",j)
 
-print("You may choose a random or systematic sweep by typing RS() or SS()")
+print("You may choose a random or systematic sweep by typing RS() or SS() \nBut I'm just gonna run RS()")
+
+RS()
