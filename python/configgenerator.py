9b0830e3b2cd197568a874659eb13e3429dbb34a
Simon Schlepphorst <dev@simon-schlepphorst.de>
2017-03-15 18:01:53 +0100

    more runs

--- python/configgenerator.py
+++ python/configgenerator.py
@@ -3,7 +3,7 @@
 import os
 import configparser
 
-def write_config(config, max_sims=10):
+def write_config(config, max_sims=10, start=0):
     #max_sims shouldn't be higher than 100
     assert max_sims <= 100
 
@@ -12,7 +12,7 @@ def write_config(config, max_sims=10):
     init = config.get('lattice', 'init')
     dirname = os.path.join("runs", algorithm, temperature, init)
     #find non-existing folder an create it
-    i = 0
+    i = start
     while i < max_sims:
         try:
             os.makedirs(os.path.join(dirname, '{:03d}'.format(i)))
@@ -37,8 +37,17 @@ config = configparser.ConfigParser()
 with open('config.ini') as f:
     config.read_file(f)
 
-T = [1.0, 1.3, 1.6, 1.8, 2.0, 2.2, 2.3] #Cip04
+#T = [1.0, 1.3, 1.6, 1.8, 2.0, 2.2, 2.3] #Cip04
 #T = [2.4, 2.6, 2.8, 3.0, 3.3, 3.6, 4.0] #Cip03
+
+# Cluster 000, 001
+#T = [1.0, 2.0, 2.3] #Cip08
+#T = [2.4, 3.0, 4.0] #Cip10
+
+# Cluster 002, 003
+#config.set('lattice', 'size', '(32,32)')
+#T = [1.0, 1.3, 1.6, 1.8, 2.0, 2.2, 2.3] #Cip06
+#T = [2.4, 2.6, 2.8, 3.0, 3.3, 3.6, 4.0] #Cip07
 
 for temp in T:
     config.set('markov chain', 'temperature', str(temp))
